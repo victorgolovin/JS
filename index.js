cd7c43c14e10df283c52000fe6f084a56dc8dd47
Victor Golovin <rinoally@gmail.com>
2025-01-17 20:05:00 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-123123
+/* JavaScript
+
+ Самое важное в JS это:
+ - Выражения
+ - Функции
+ - Объекты 
+
+ Пример обьекта:
+
+ {
+  visible: true,
+  colorDepth: 24,
+  title: 'My Image',
+  orientation: {
+    angle: 0,
+    type: 'landscape'
+  },
+  pixelDepth: 24,
+  width: 1440
+ }
+ */
