0be8fbc9e9b0650c8f20fd79a9d2b4051d1ca506
Victor Golovin <rinoally@gmail.com>
2025-01-20 04:56:39 +0300

    learn type object pt2

--- index.js
+++ index.js
@@ -29,6 +29,8 @@
   a <= b || c !== d  // true или false
   myFunction(c,d)  // результат функции */
 
+  // ----------------------------------------------------
+
  // let myName
 
  // console.log(myName)
@@ -43,15 +45,163 @@
 
  // Ссылочный тип Object
 
- const objectA = {
-  a: 10,
-  b: true
- }
+ // ----------------------------------------------------
 
- const copyOfA = objectA
+//  const objectA = {
+//   a: 10,
+//   b: true
+//  }
 
- copyOfA.a = 20
- copyOfA.c = 'abc'
+//  const copyOfA = objectA
 
- console.log(objectA)
- console.log(copyOfA)
+//  copyOfA.a = 20
+//  copyOfA.c = 'abc'
+
+//  console.log(objectA)
+//  console.log(copyOfA)
+
+// ----------------------------------------------------
+ 
+// Динамическая типизация в JS
+
+//  function a() {
+//   console.log('Hey there')
+//  }
+
+//  a()
+
+//  a = 10
+
+//  a() // TypeError: a is not a function
+
+// ----------------------------------------------------
+
+// Пример с CONST
+
+// const a = () => {
+//   console.log('Hey there')
+// }
+
+// a()
+
+// a = 10 // TypeError: Assignment to constant variable.
+
+// a()
+
+// Переменной а нельзя присвоить новое значение
+
+// ----------------------------------------------------
+
+// const myCity = {
+//   city: 'New York',
+//   popular: true,
+//   country: 'USA'
+// }
+
+// console.log(myCity.city) // New York
+
+// console.log(myCity.popular) // true
+
+// ----------------------------------------------------
+
+// Так же можешь менять свойства обьекта
+
+// const myCity = {
+//   city: 'New York'
+// }
+
+// myCity.city = 'Las Vegas'
+
+// console.log(myCity) // { city: 'Las Vegas' }
+
+// ----------------------------------------------------
+
+// Так же можем создавать новые свойства в обьектах
+
+// const myCity = {
+//   city: 'New York'
+// }
+
+// myCity.popular = true
+
+// console.log(myCity) // { city: 'New York', popular: true }
+
+// myCity.country = 'USA'
+
+// console.log(myCity) // { city: 'New York', popular: true, country: 'USA' }
+
+// ----------------------------------------------------
+
+// Теперь рассмотрим пример удаления свойства из обьекта
+
+// const myCity = {
+//   city: 'New York',
+//   popular: true,
+//   country: 'USA'
+// }
+
+// delete myCity.country
+
+// console.log(myCity) // { city: 'New York', popular: true }
+
+// ----------------------------------------------------
+
+// Доступ к значению свойства с помощью квадратных скобок
+
+// const myCity = {
+//   city: 'New York'
+// }
+
+// myCity['popular'] = true
+
+// console.log(myCity) // { city: 'New York', popular: true }
+
+// const countryPropertyName = 'country'
+
+// myCity[countryPropertyName] = 'USA'
+
+// console.log(myCity) // { city: 'New York', popular: true, country: 'USA' }
+
+// ----------------------------------------------------
+
+// Так же есть обьекты с вложенными свойствами 
+
+// const myCity = {
+//   city: 'New York',
+//   info: {
+//     isPopular: true,
+//     country: 'USA'
+//   }
+// }
+
+// console.log(myCity.info.isPopular) // true
+
+// delete myCity.info['isPopular']
+
+// console.log(myCity) // { city: 'New York', info: { country: 'USA' } }
+
+// ----------------------------------------------------
+
+// Мы можем использовать переменные в обьектах
+
+// const name = 'Victor'
+// const postQty = 25
+
+// const useProfile = {
+//   name: name,
+//   postQty: postQty,
+//   hasSignedAgreement: false
+// }
+
+// Или сокращенный вариант который чаще всего используется
+
+const name = 'Victor'
+const postQty = 25
+
+const useProfile = {
+  name,
+  postQty,
+  hasSignedAgreement: false
+}
+
+console.log(useProfile) // { name: 'Victor', postQty: 25, hasSignedAgreement: false }
