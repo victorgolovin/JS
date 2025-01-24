4d03ab1410746c88bf8dfba535a02456e24264d3
Victor Golovin <rinoally@gmail.com>
2025-01-24 21:50:00 +0300

    add commit

--- index-function.js
+++ index-function.js
@@ -36,16 +36,72 @@
 
 // Функция - это обьект 
 
-function myFn (a, b) {
-    let c 
-    a = a + 1
-    c = a + b 
-    return c
+// function myFn (a, b) {
+//     let c 
+//     a = a + 1
+//     c = a + b 
+//     return c
+// }
+
+// myFn(10, 3)
+
+// const result = myFn(10, 3)
+// console.log(result) // 14
+
+
+// Передача значения по ссылке 
+
+// const personOne = {
+//     name: 'Bob',
+//     age: 21
+// }
+
+// function increasePersonAge(person) {
+//     person.age += 1
+//     return person
+// }
+
+// increasePersonAge(personOne)
+// console.log(personOne.age) // 22
+
+// Внутри функции не рекомендуется мутировать внешние обьекты
+// Но мы можем внутри функции создавать копии обьектов
+
+// const personOne = {
+//     name: 'Bob',
+//     age: 21
+// }
+
+// function increasePersonAge(person) {
+//     const updatedPerson = Object.assign({}, person)
+//     updatedPerson.age += 1
+//     return updatedPerson
+// }
+
+// const updatedPersonOne = increasePersonAge(personOne)
+
+// console.log(personOne.age) // 21
+// console.log(updatedPersonOne.age) // 22
+
+// Пример выше как нужно создавать копии обьектов внутри функции и мутировать их
+// при этом не трогая оригинальный (внешний) обьект
+
+// Колбэк функции
+
+// function anotherFunction() {
+
+// }
+
+// function fnWithCallback(callbackFunction) {
+//     callbackFunction()
+// }
+
+// fnWithCallback(anotherFunction)
+
+// В теле функции вызывается другая функция, ниже еще пример
+
+function printMyName() {
+    console.log('Victor')
 }
 
-myFn(10, 3)
-
-const result = myFn(10, 3)
-console.log(result) // 14
-
-
+setTimeout(printMyName, 1000)
