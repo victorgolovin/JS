60a12c615c034c7d5cd028d7a358de8ab0db9193
Victor Golovin <rinoally@gmail.com>
2025-01-31 22:22:21 +0300

    add commit

--- index-string-concatenation.js
+++ index-string-concatenation.js
@@ -1,2 +1,29 @@
 // Конкатенация строк
 
+
+// Оператор + для конкатенации строк
+
+// 'Hello ' + 'World'
+
+// ----------------------------------------------------
+
+// Переменные в конкатенации строк
+
+// const hello = 'Hello'
+// const world = 'World'
+
+// const greeting = hello + '' + world
+
+// Но лучше использовать шаблонные строки
+
+const hello = 'Hello'
+const world = 'World'
+
+const greeting = `${hello} ${world}` // в фигурных скобках может быть любое выражение, например, вызов функции
+
+const myName = 'Victor'
+const myCity = 'Moscow'
+
+const myGreetings = `${'Me name'} ${myName} ${'and i live in'} ${myCity}`
+
+console.log(myGreetings)
