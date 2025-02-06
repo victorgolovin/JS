dfc91784808dfc4a826bf19aef693962f3bb861e
Victor Golovin <rinoally@gmail.com>
2025-02-06 22:45:05 +0300

    add commit

--- index-errors.js
+++ index-errors.js
@@ -1,9 +1,35 @@
 // Что происходит в случае ошибок 
 
+// const fnWithError = () => {
+//     throw new Error('Some Error')
+// }
+
+// fnWithError()
+
+// console.log('Continue...')
+
+// ----------------------------------------------------
+
+// TRY/CATCH поможет обнаружить ошибку
+
+try {
+    // Выполнение блока кода
+} catch (error) {
+    // Этот блок выполняется в случае 
+    // возникновения ошибок в блоке try
+}
+
+// Ниже пример
+
 const fnWithError = () => {
     throw new Error('Some Error')
 }
 
-fnWithError()
+try {
+    fnWithError()
+} catch (error) {
+    console.error(error)
+    console.log(error.message)
+}
 
 console.log('Continue...')
