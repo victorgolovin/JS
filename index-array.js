facf706c79f71bd7e58717f572b5f68586287839
Victor Golovin <rinoally@gmail.com>
2025-02-13 20:47:34 +0300

    add commit

--- index-array.js
+++ index-array.js
@@ -2,11 +2,13 @@
 
 // Формат записи массивов 
 
-const myArray = [1, 2, 3]
-console.log(myArray) // [ 1, 2, 3 ]
+// const myArray = [1, 2, 3]
+// console.log(myArray) // [ 1, 2, 3 ]
 
-const myArray2 = new Array(1, 2, 3)
-console.log(myArray2) // [ 1, 2, 3 ]
+// const myArray2 = new Array(1, 2, 3)
+// console.log(myArray2) // [ 1, 2, 3 ]
+
+// ----------------------------------------------------
 
 // Свойство массива
 
@@ -16,3 +18,32 @@ console.log(myArray2) // [ 1, 2, 3 ]
 // 1: 2
 // 2: 3
 // length: 3
+
+// ----------------------------------------------------
+
+// Массив VS Обьект
+
+// const myObject = {
+//     0: 1,
+//     1: 2,
+//     2: 3,
+//     length: 3
+// }
+
+// console.log(myObject) // { '0': 1, '1': 2, '2': 3, length: 3 }
+
+// const myArray = [1, 2, 3]
+
+// console.log(myArray) // [ 1, 2, 3 ]
+
+// ----------------------------------------------------
+
+// Чтение значений массива
+
+const myArray = [1, true, 'a']
+console.log(myArray) // [ 1, true, 'a' ]
+
+console.log(myArray[0]) // 1
+console.log(myArray[1]) // true
+
+console.log(myArray.length) // 3
